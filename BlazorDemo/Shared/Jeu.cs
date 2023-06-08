0d24b3a8f67193be2004d7d3b13fb9d72df48311
imrainmaker <bazinjeremy@gmail.com>
2023-06-08 17:27:42 +0200

    exo form

---
+++ BlazorDemo/Shared/Jeu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Shared
+{
+    public class Jeu
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public DateTime DatedeSortie { get; set; }
+        [Range(0, 5)]
+        public int Note { get; set; }
+    }
+}
